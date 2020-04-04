ce2d19c22c0ee7a51dc0ad675e3d76752797ba2f
Khurram <khurramshahzad@tfl.gov.uk>
2020-04-04 22:08:00 +0100

    change example

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,21 +1,51 @@
 exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
-  const pokemons = [
-    { name: "Pikachu", type: "electric" },
-    { name: "Squirtle", type: "water" },
-  ]
 
-  pokemons.forEach(pokemon => {
-    const node = {
-      name: pokemon.name,
-      type: pokemon.type,
-      id: createNodeId(`Pokemon-${pokemon.name}`),
-      internal: {
-        type: "Pokemon",
-        contentDigest: createContentDigest(pokemon),
-      },
+  const { createNode } = actions
+
+  // Data can come from anywhere, but for now create it manually
+  const myData = {
+    key: 123,
+    foo: `The foo field of my node`,
+    bar: `Baz`
+  }
+  const myData1 = {
+    key: 1234,
+    foo: `The foo field of my node`,
+    bar: `Baz`
+  }
+
+  const nodeContent = JSON.stringify(myData)
+
+  const nodeMeta = {
+    id: createNodeId(`my-data-${myData.key}`),
+    parent: null,
+    children: [],
+    internal: {
+      type: `MyNodeType`,
+      mediaType: `text/html`,
+      content: nodeContent,
+      contentDigest: createContentDigest(myData)
     }
-    actions.createNode(node)
-  })
+  }
+
+  const node = Object.assign({}, myData, nodeMeta)
+  createNode(node)
+
+
+  const nodeMeta1 = {
+    id: createNodeId(`my-data-${myData1.key}`),
+    parent: null,
+    children: [],
+    internal: {
+      type: `MyNodeType1`,
+      mediaType: `text/html`,
+      content: nodeContent,
+      contentDigest: createContentDigest(myData1)
+    }
+  }
+
+  const node1 = Object.assign({}, myData1, nodeMeta1)
+  createNode(node1)
 }
 
 
